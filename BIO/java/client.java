4fdebd7afa25e20054f2301cff22fcbbc03fc980
Peter Hua <diskrooms@gmail.com>
2021-07-27 23:57:32 +0800

    fix

---
+++ BIO/java/client.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class client {
+    public static void main(String[] args)  throws IOException{
+
+        Socket sock = new Socket(InetAddress.getLocalHost(),8888);
+        //System.out.println("客户端sock"+sock.getClass());
+        OutputStream outputStream = sock.getOutputStream();
+        outputStream.write("hello server".getBytes());
+        sock.shutdownOutput();
+        //
+        InputStream inputStream = sock.getInputStream();
+        byte[] buffer = new byte[1000];  //准备一个瓢
+        int readCount = 0;
+
+        while ((readCount = inputStream.read(buffer)) != -1) {   //
+            //byteArrayOutputStream.write(buffer, 0, readCount);
+            System.out.println(new String(buffer, 0, readCount));
+        }
+        outputStream.close();
+        inputStream.close();
+        sock.close();
+    }
+}
---
+++ BIO/java/sever.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class sever {
+    public static void main(String[] args) throws IOException {
+        ServerSocket sersock = new ServerSocket(8888);
+        /*while (true) {*/
+            Socket socket = sersock.accept();
+            //System.out.println("服务端sock" + socket.getClass());
+            InputStream inputStream = socket.getInputStream();
+            byte[] buffer = new byte[1000];  //准备一个瓢
+            int readCount = 0;
+            while ((readCount = inputStream.read(buffer)) != -1) {   //一直舀
+                //byteArrayOutputStream.write(buffer, 0, readCount);
+                System.out.println(new String(buffer, 0, readCount));
+            }
+
+            //inputStream.close();
+            //
+
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("hello client".getBytes());
+            socket.shutdownOutput();
+
+            outputStream.close();
+            inputStream.close();
+            socket.close();
+        /*}*/
+        //sersock.close();
+    }
+}
